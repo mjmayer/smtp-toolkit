57f1fd4294950288027db17c304a6a7422430512
Michael Mayer <mjmayer@gmail.com>
2020-03-05 08:40:21 -0800

    Add script for sending smtp message

---
+++ send_smtp_attachment.py
@@ -0,0 +1,32 @@
+import smtplib
+import argparse
+import uuid
+import time
+from email.message import EmailMessage
+
+parser = argparse.ArgumentParser(description="Sends test emails")
+parser.add_argument("-u", "--user", help="SMTP username" )
+parser.add_argument("-p", "--password", help="SMTP Password")
+parser.add_argument("--server", help="hostname for SMTP server")
+parser.add_argument("--port", default=587, type=int, help="SMTP server port")
+parser.add_argument("--to", default=str(uuid.uuid1()) + "@mailsac.com")
+parser.add_argument("--sender", default=str(uuid.uuid1()) + "@mailsac.com")
+parser.add_argument("--subject", default="Test Message" + str(time.gmtime()))
+args = parser.parse_args()
+
+msg = EmailMessage()
+msg.set_content("Email Message Body")
+msg['Subject'] = "Test Email" + args.subject
+msg['From'] = args.to
+msg['To'] = args.sender
+
+server = smtplib.SMTP(args.server, 587)
+server.connect()
+server.starttls()
+server.login(args.user, args.password)
+server.send_message(msg)
+server.quit()
+
+print("To: " + args.to + "\r\n" + "From: " + args.sender + "\r\n" + "Subject: "
+     + args.subject + "\r\n" + "Inbox: " + "https://mailsa.com/inbox/" +
+     args.to)
